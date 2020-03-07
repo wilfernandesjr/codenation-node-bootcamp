d060b64195880b7923752681186a1a70fcf1fe5e
wilfernandesjr <wilmar.wfsj@gmail.com>
2020-03-07 16:29:15 -0300

    Add solução exercicio em sala

--- modulo-8/exemplos/auth/app.js
+++ modulo-8/exemplos/auth/app.js
@@ -1,30 +1,103 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
 
-const routes = require('./routes')
+const { getHashedPassword, generateAuthToken } = require('./utils')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cookieParser())
 
 const users = []
 
-const isAuthorized = (req, res, next) => {
+const isValid = (username, password, confirmPassword, age) => {
+  if (
+    !username ||
+    !password ||
+    !confirmPassword ||
+    !age
+  ) return false
 
+  if (
+    password !== confirmPassword
+  ) return false
+
+  if (users.some(user => user.username === username)) return false
+
+  return true
 }
 
-router.get('/signup', (req, res, next) => {
+app.post('/signup', (req, res, next) => {
+  const {
+    username,
+    password,
+    confirmPassword,
+    age
+  } = req.body
 
+  if (!isValid(username, password, confirmPassword, age)) {
+    return res.status(400).json({
+      error: 'Invalid parameters.'
+    })
+  }
+
+  const hashedPassword = getHashedPassword(password)
+  const token = generateAuthToken()
+
+  users.push({
+    username, hashedPassword, age, token
+  })
+
+  return res.status(201).json({ username, age, token })
 })
 
-router.get('/signin', (req, res, next) => {
+app.post('/signin', (req, res, next) => {
+  const { username, password } = req.body
 
+  const user = users.find(u => (
+    u.username === username &&
+    u.hashedPassword === getHashedPassword(password)
+  ))
+
+  if (!user) return res.status(401).json({
+    error: 'User or password invalid'
+  })
+
+  return res.status (200).json({
+    token: user.token
+  })
 })
 
-router.get('/buy-drinks', isAuthorized, (req, res, next) => {
+const isAuthorized = (req, res, next) => {
+  const token = req.get('x-auth-token')
 
+  const user = users.find(u => u.token === token)
+
+  if ((user || {}).age >= 18) return next()
+
+  return res.status(401).json({
+    error: 'Sai daqui menó'
+  })
+}
+
+app.get('/alcoholic-drinks', isAuthorized, (req, res, next) => {
+  return res.status(200).json([
+    {
+      name: 'Catuaba',
+      grad: '15%',
+      rating: 1.5
+    },
+    {
+      name: 'Corote Sabores',
+      grad: '25%',
+      rating: 6.5
+    },
+    {
+      name: 'Pitú',
+      grad: '55%',
+      rating: 0.5
+    }
+  ])
 })
 
 app.listen(3000)
